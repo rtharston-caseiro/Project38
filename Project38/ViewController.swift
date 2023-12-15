e0f7c4ee686420a31638f8ace197d3d5a79bb387
Reed Harston <reed@caseiro.software>
2023-12-15 12:16:40 -0800

    Add core data container to view controller

--- Project38/ViewController.swift
+++ Project38/ViewController.swift
@@ -5,15 +5,32 @@
 //  Created by Caseiro Dev on 12/14/23.
 //
 
+import CoreData
 import UIKit
 
 class ViewController: UITableViewController {
+    var container: NSPersistentContainer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        container = NSPersistentContainer(name: "Project38")
+        container.loadPersistentStores { storeDescription, error in
+//            print("sqlite url: \(String(describing: storeDescription.url))")
+            if let error {
+                print("Unresolved error \(error)")
+            }
+        }
     }
 
-
+    func saveContext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occurred while saving: \(error)")
+            }
+        }
+    }
 }
 
